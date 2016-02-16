d3eb89764bf5716206224bfb71a4d19169f486ea
jmitt508 <mittonjw@slu.edu>
2016-02-15 21:19:05 -0600

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -8,41 +8,41 @@ public class Calendar {
      * The constructor for Calendar.
      */
     public Caledar(){
-        cal = new HashMap<Date, List>();
+        cal = new HashMap<date, List>();
     }
     /**
-     * Creates a new appointment and adds it to the Hashmap of Appointments.
+     * Creates a new appointment and adds it to the HashMap of Appointments.
      * @param startTim A DateTime object that is the starting time of the appointment.
      * @param endTim A DateTime object that is the ending time of the appointment.
      * @param desc A string that is the description of the appointment.
-     * @param date A Date that is the date the appointment takes place.
      */
-    @SuppressWarnings("empty-statement")
-    public void addApt(DateTime startTim, DateTime endTim, String desc, Date date)
+    public void addApt(dateTime startTim, dateTime endTim, String desc)
     {
         Appointment apt = new Appointment(startTim, endTim, desc);
-        if(cal.containsKey(date)){
-            cal.getClass(date).add(apt);
+        if(cal.containsKey(startTim.getDate())){
+            cal.getClass(startTim.getDate()).add(apt);
             
         }else{
             List y;
             y.add(apt);
-            cal.put(date, y);
+            cal.put(startTim.getDate(), y);
         }
     }
     /**
      * Deletes a selected appointment from the book.
-     * @param apt The appointment that is desired to be deleted.
-     * @param dat A Date that is the date of the appointment that is to be removed.
+     * @param startTim A DateTime object that is the starting time of the appointment.
+     * @param endTim A DateTime object that is the ending time of the appointment.
+     * @param desc A string that is the description of the appointment.
      */
-    public void deleteApt(Appointment apt, Date dat)
+    public void deleteApt(dateTime startTim, dateTime endTim, String desc)
     {
-        List z = cal.get(dat);
+        Appointment apt = new Appointment(startTim, endTim, desc);
+        List z = cal.get(startTim.getDate());
         boolean flag = true;
         int i = 0;
         while(flag){
-           if(z.get(i)==apt){
-               z.remove(apt);
+           if(z.get(i).getDescription()==desc && z.get(i).getStartTime() == startTim ){
+               z.remove(i);
                flag = false;
            }
            i++;
@@ -53,7 +53,7 @@ public class Calendar {
      * Prints out all of the appointments for a given day.
      * @param date A Date that is the desired day that the user wants the appointments for.
      */
-    public void printApt(Date date)
+    public void printApt(date date)
     {
         //Date date = Date(dat);
         if(cal.containsKey(date)){
